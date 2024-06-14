3ee8303f266806b0224ec10ebea25052be53e9e8
Melissa Freese <melissafreese@gmx.com>
2024-06-14 13:08:44 -0700

    Fix bug for checkbox input

--- scripts.js
+++ scripts.js
@@ -1,16 +1,29 @@
+// define button and form
+const popUpForm = document.getElementById("popUpForm");
+const addBookForm = document.getElementById("addBookForm");
+let button = document.getElementById("addNewBook");
+
+
+// form pop-up
+button.addEventListener("click", function () {
+	document.getElementById("popUpForm").style.display = "block";
+});
+
+//form submission
+addBookForm.addEventListener("submit", function (e) {
+	e.preventDefault(); // prevent the form submission from refreshing the page
+	addBookToLibrary();
+});
+
 // constructor function
 function Book(title, author, pages, isbn, read) {
 	(this.title = title),
 		(this.author = author),
 		(this.pages = pages),
 		(this.isbn = isbn),
-		(this.read = read),
-		(this.info = function () {
-			return `${this.title} by ${this.author},${this.isbn} ${this.pages} 
-			pages, ${this.read}.`;
-		});
+		(this.read = read);
 }
-// function to add books to the library/ myLibrary array
+// function to add books to the library
 function addBookToLibrary() {
 	// initialize empty array
 	const myLibrary = [];
@@ -20,40 +33,33 @@ function addBookToLibrary() {
 	const author = document.getElementById("author");
 	const pages = document.getElementById("pages");
 	const isbn = document.getElementById("isbn");
-	const read = document.getElementById("read");
+	const read = document.getElementById("read-status");
 
 	// get the values of the input elements
 	const titleValue = title.value;
 	const authorValue = author.value;
 	const pagesValue = pages.value;
 	const isbnValue = isbn.value;
-	const readValue = read.value;
+	const readValue = read.checked;
 
 	// add the values to the array
 	myLibrary.push(
 		new Book(titleValue, authorValue, pagesValue, isbnValue, readValue)
 	);
 
-	console.log(myLibrary);
+	    // Log the book's details to the console
+		console.log(`Title: ${titleValue}`);
+		console.log(`Author: ${authorValue}`);
+		console.log(`Pages: ${pagesValue}`);
+		console.log(`ISBN: ${isbnValue}`);
+		console.log(`Read: ${readValue}`);
+	
 
 	// clear the input fields
-	title.value = "";
-	author.value = "";
-	pages.value = "";
-	isbn.value = "";
-	read.value = "";
+	// title.value = "";
+	// author.value = "";
+	// pages.value = "";
+	// isbn.value = "";
+	// read.value = false;
 }
 
-const twilight = new Book("Twilight", "J.K.Rowling", 300, 1234567890, "Yes");
-
-console.log(twilight);
-console.log(twilight.info());
-
-// // define button and form
-const popUpForm = document.getElementById("popUpForm");
-let button = document.getElementById("addNewBook");
-
-// form pop-up
-button.addEventListener("click", function () {
-	document.getElementById("popUpForm").style.display = "block";
-});
